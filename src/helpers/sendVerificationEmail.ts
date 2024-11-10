7bb4eca3124a87ed88b666a559cae70409afc52e
Sanyam Pandey <sanyam071104@gmail.com>
2024-11-11 00:43:24 +0530

    deployment changes

--- src/helpers/sendVerificationEmail.ts
+++ src/helpers/sendVerificationEmail.ts
@@ -8,16 +8,23 @@ export async function sendVerificationEmail(
     verifyCode: string
 ): Promise<ApiResponse> {
     try {
+        console.log(email)
 
         const { data, error } = await resend.emails.send({
             from: 'Acme <onboarding@resend.dev>',
-            to: email,
+            to: 'sanyam071104@gmail.com',
             subject: 'Mystry Message | Verification Code',
             react: VerificationEmail({ username : username, otp: verifyCode }),
         });
 
+        console.log(`USername === > ${username}, ${verifyCode}`);
+        
+
         if (error) {
+            console.log(error);
             return { success: false, message: "Failed to send verification email" }
+            
+            
         }
 
         console.log(data)
